b2b6f1d7f7f1d9366da9cd14d6a3692f6c107fed
Nadezhda Ugay <777ugay@gmail.com>
2023-10-21 20:00:37 +0300

    add dog, parent, breed

---
+++ shelter/models.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=250, verbose_name='кличка')
+    photo = models.ImageField(upload_to='photo/', verbose_name='фото', **NULLABLE)
+    birth = models.DateField(verbose_name='ДР', **NULLABLE)
+    breed = models.ForeignKey('Breed', on_delete=models.CASCADE, verbose_name="порода", **NULLABLE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="автор", **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'собака'
+        verbose_name_plural = 'собаки'
+        ordering = ('name', 'author',)
+
+
+class Parent(models.Model):
+    name = models.CharField(max_length=250, verbose_name='кличка')
+    breed = models.ForeignKey('Breed', on_delete=models.CASCADE, verbose_name="порода", **NULLABLE)
+    dog = models.ForeignKey('Dog', on_delete=models.CASCADE, verbose_name='собака')
+    birth = models.DateField(verbose_name='ДР', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} (предок {self.dog})'
+
+    class Meta:
+        verbose_name = 'предок'
+        verbose_name_plural = 'предки'
+
+
+class Breed(models.Model):
+    breed = models.CharField(max_length=250, verbose_name='порода')
+    description = models.CharField(max_length=250, verbose_name='Описание', **NULLABLE)
+    pic = models.ImageField(upload_to='pics/', verbose_name='Картинка', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.breed}'
+
+    class Meta:
+        verbose_name = 'порода'
+        verbose_name_plural = 'породы'
--- users/models.py
+++ users/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from shelter.models import NULLABLE
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.CharField(verbose_name='email', unique=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    phone = models.CharField(verbose_name='телефон', max_length=35, **NULLABLE)
+    telegram = models.CharField(verbose_name='телеграм ник', max_length=50, **NULLABLE)
+    avatar = models.ImageField(verbose_name='аватар', upload_to='users/', **NULLABLE)
